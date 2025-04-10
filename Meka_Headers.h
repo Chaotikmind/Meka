45f57ce16c84b71e07e8068aa396f6cdf8668eda
alix.leteur <alix.leteur@mksinst.com>
2025-04-10 20:11:53 +0200

    Basic typedefs for clarity and ease of typing

---
+++ Meka_Headers.h
@@ -0,0 +1,16 @@
+#include <ctypes.h>
+
+typedef uint8_t u8;
+typedef int8_t i8;
+typedef uint16_t u16;
+typedef int16_t u16;
+typedef uint32_t u32;
+typedef int16_t i32;
+typedef uint64_t u64;
+typedef int64_t i64;
+
+//typedef uint128_t u128;
+//typedef int128_t i128;
+
+typedef float f32;
+typedef double f64;
